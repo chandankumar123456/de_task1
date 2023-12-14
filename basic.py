dc1bb118ec2275e94f1f0ab2af20b66359cfb48a
Vanjarapu Chandan Kumar <43309474+chandankumar123456@users.noreply.github.com>
2023-12-14 20:23:06 +0530

    Added Task 1 Files via upload

---
+++ basic.py
@@ -0,0 +1,25 @@
+print("Hello, World!")
+"""
+This is a multiline  comment
+It does not interfere with the code
+"""
+#This is a single line comment
+#camelcasing variable declaration 
+yearOfStudying = 2
+print("My Year of studying is ", yearOfStudying)
+name = "Chandan Kumar" #string
+cgpa = 9.13 #floating point number
+roll = 23 #integer
+ans = True #boolean data type
+#Type Casitng
+cgpaInteger = int(cgpa)
+print(type(cgpa))
+print(type(cgpaInteger))
+#String Slicing
+slicedString = name[:7]
+print(slicedString)
+#Methods in python
+print(name.upper())
+print(name.lower())
+print(name.capitalize())
+print(name.index("C"))  
